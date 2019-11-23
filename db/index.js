b6e1eade485acf8fa8a37442a95d792dbd8d8941
Blake Mason <blakelmason@gmail.com>
2019-11-23 15:53:24 -0800

    Heroku ready.

--- db/index.js
+++ db/index.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql')
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '4808',
-  database: 'grocery-list'
-})
+let connect
+
+if (process.env.NODE_ENV === 'production') {
+  connect = process.env.JAWSDB_URL
+} else {
+  connect = {
+    host: 'localhost',
+    user: 'root',
+    password: '4808',
+    database: 'grocery-list'
+  }
+}
+
+var connection = mysql.createConnection(connect)
 
 connection.connect()
 
--- server/index.js
+++ server/index.js
@@ -34,10 +34,6 @@ app.delete('/groceries', (req, res) => {
   })
 })
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
-)
-
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`))
